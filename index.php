95699c06b7bdf37ec7cc2cf7ba10f3c933bed475
Albin-B7 <eaglealbin@gmail.com>
2025-01-25 16:40:54 +0100

    first php lesson

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// $school = "Digital School";
+// $age = 19;
+
+// echo "I love $school";
+// echo "my age is $age";
+
+
+// $x = 120;
+// $y = 20;
+
+// echo $x * $y;
+
+
+
+$name = "Albin";
+$surname = "Bajrami";
+
+$fullname = $name . " " . $surname;
+
+echo $fullname;
+echo strlen($name);
+echo str_word_count($name);
+echo strrev($name);
+
+?>
