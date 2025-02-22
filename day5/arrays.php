943829e6cb80d707442fd9c2b9683f00643a4abb
Albin-B7 <eaglealbin@gmail.com>
2025-02-22 07:38:16 -0800

    day5

---
+++ day5/arrays.php
@@ -0,0 +1,23 @@
+<?php
+    $grade = array(
+        "Math" => "4",
+        "art" => "5",
+        "History" => "3",
+        "Music" => "5",
+    );
+
+
+        foreach($grade as $subject => $grade){
+            echo "Subject:  ". $subject . "->   Grade ". $grade;
+            echo "<br>";
+        } 
+
+
+
+
+
+
+
+
+
+?>
