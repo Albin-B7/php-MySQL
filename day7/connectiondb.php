ef0f43d3669fbeca6860639f1e2b2487411e19a9
Albin-B7 <eaglealbin@gmail.com>
2025-03-08 07:45:44 -0800

    c3

--- day7/connectiondb.php
+++ day7/connectiondb.php
@@ -1,13 +1,17 @@
 <?php
 $host = "localhost";
+$db = "Albin";
 $user = "root";
 $pass = "";
 
 
 try{
     $conn = new PDO("mysql:host=$host;" ,$user ,$pass);
+    $sql = "CREATE TABLE users (id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(30) NOT NULL,
+            password VARCHAR(30) NOT NULL)";
 
-    echo "Connected";
+    echo "Table created successfully";
 }catch(Exception $e){
 
     echo "Not Connected";
