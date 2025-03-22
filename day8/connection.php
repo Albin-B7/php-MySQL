5b5d01a320a10802cf78d5f86358a32591b8d2f2
Albin-B7 <eaglealbin@gmail.com>
2025-03-22 08:16:36 -0700

    day8 final

--- day8/connection.php
+++ day8/connection.php
@@ -1,15 +1,23 @@
 <?php
     try{
-        $pdo = new PDO("mysql:host=localhost;dbname=database", "root", "");
+        $pdo = new PDO("mysql:host=localhost;dbname=albin", "root", "");
+
         $username = "Albin";
         $password = password_hash("mypassword", PASSWORD_DEFAULT);
+        // $email = "test@gmail.com";
 
-        $sql = "INSERT INTO users(username,password) VALUES ("$username", "$password")";
+        $sql = "INSERT INTO users(username,password) VALUES ('$username', '$password')";
+
+        // $sql= "ALTER table users ADD email VARCHAR(255)";
+        // $sql = "ALTER table users DROP COLUMN email";
+        // $sql = "DROP TABLE PRODUCTS";
+
+
         $pdo->exec($sql);
 
         echo "New record created succssefully";
     }catch(DOExection $e){
-        echo $->getMessage();
+        echo $e->getMessage();
     }
 
 
